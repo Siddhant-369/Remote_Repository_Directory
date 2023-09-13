dc7a3893656dbed50bcf8d454787e8f725ab7c34
Siddhant-369 <siddhantchahande2001@gmail.com>
2023-09-13 13:02:33 +0530

    Delete several records/codes in mysparkcode.py and added requirement.txt

--- mysparkcode.py
+++ mysparkcode.py
@@ -12,11 +12,3 @@ df=spark.read.format('json').option("mode",'DROPMALFORMED').load(data) #Bcoz we 
 
 """Observe, the 'loc' column's data type is array, So, i wan't to process this array format then,
 first array data you must make structure. How to make structure is as follows,"""
-#All these operations are called as Data Cleaning Steps.
-df=df.withColumn("loc", explode(col('loc'))).withColumnRenamed('_id','id')\
-    .withColumn('id',col('id').cast(IntegerType())) \
-    .withColumn('pop',col('pop').cast(IntegerType()))\
-    .withColumn('loc',abs(col("loc")))
-#Now, array data you'll get in each row every row.
-df.show()
-df.printSch
